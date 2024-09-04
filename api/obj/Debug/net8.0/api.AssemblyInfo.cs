9933040ae9c0c1e95f568992d7fcc3ab206e203f
ErickAC12 <112605408+ErickAC12@users.noreply.github.com>
2024-09-04 17:10:22 -0600

    comment getAllapi

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4173318e2d78c291449697ea5b434a9c888be62d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27e34a9ac5155c95ee3a8b326e1e78f0f9e977f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
