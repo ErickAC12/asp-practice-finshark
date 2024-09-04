27e34a9ac5155c95ee3a8b326e1e78f0f9e977f5
ErickAC12 <112605408+ErickAC12@users.noreply.github.com>
2024-09-03 19:51:18 -0600

    refactor to repository

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8bbb20f3db9e9f1afe6d166abf5481cd1b36850")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4173318e2d78c291449697ea5b434a9c888be62d")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
