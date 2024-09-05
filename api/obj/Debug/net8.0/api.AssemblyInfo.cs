913c2401a15ee49fe208bd903ff8c8c53f87b4ee
ErickAC12 <112605408+ErickAC12@users.noreply.github.com>
2024-09-05 17:05:24 -0600

    sorting on query

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d26cd69a4f126f53dfe1fdb4d429c5b02c0abb0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c6b0ed49f6c59d16d5d872a4d33d66a1ab86213")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
