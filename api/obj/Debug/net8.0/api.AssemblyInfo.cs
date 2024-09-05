8d26cd69a4f126f53dfe1fdb4d429c5b02c0abb0
ErickAC12 <112605408+ErickAC12@users.noreply.github.com>
2024-09-05 16:36:20 -0600

    data validation

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+045bfa54dc8d75b2ffeb9c4183c2daf9f9d59678")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f85cc2ba6d2d25c40bb6411026319ab4043d31a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
