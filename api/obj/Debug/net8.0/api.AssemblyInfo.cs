1279d649025f27b2d47ae17e650c2f0d77c49c34
ErickAC12 <112605408+ErickAC12@users.noreply.github.com>
2024-09-06 17:49:06 -0600

    login authorization

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dd26dfcd1594bb3afeee52d522c822aec54abaf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9f31c084568714ea4cab34bfe274c0ef175707f")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
